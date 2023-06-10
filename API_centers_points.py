fc7bdbb9767928c385e2a29919721ba0bc53fe0c
yukuznetsova <115005812+yukuznetsova@users.noreply.github.com>
2023-06-10 18:52:10 +0300

    Update API_centers_points.py

--- API_centers_points.py
+++ API_centers_points.py
@@ -9,9 +9,9 @@ import geopandas as gpd
 #!pip install folium matplotlib mapclassify
 #!pip install geographiclib
 
-data_centers = requests.get("https://apidata.mos.ru/v1/datasets/605/features?api_key=0c4259c55453af65f9b7052058e0bf28")
-centers = data_centers.json()
-centers_points = gpd.GeoDataFrame.from_features(centers, crs="EPSG:4326")
-centers_points.to_csv('psich_centers_points.csv')
+data_centers = requests.get("https://apidata.mos.ru/v1/datasets/605/features?api_key=0c4259c55453af65f9b7052058e0bf28") #загружаю данные с data.mos.ru, используя API
+centers = data_centers.json() #преобразование в json
+centers_points = gpd.GeoDataFrame.from_features(centers, crs="EPSG:4326") #создаю из json датафрейм
+centers_points.to_csv('psich_centers_points.csv') #загружаю датафрейм
 
 centers_points.explore()
